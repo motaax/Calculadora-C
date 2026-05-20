4b47e16c2a8dfb71a46b16b9c8435be3078eb6ce
Felipe Mota <felipemota465@gmail.com>
2026-05-20 11:30:58 -0300

    Calculadora

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # Calculadora Baseada em Texto em C
+
+Ao executar o programa, ele exibirá as seguintes opções:
+
+![menu](imgs/menu.png)
+
+Se o usuário escolher uma operação(por exemplo, "1" para adição), o programa solicitará dois números:
+
+![operação](imgs/operacao.png)
+
+Após exibir o resultado, o programa perguntará se o usuário deseja realizar outra operação:
+
+![novamente](imgs/novamente.png)
+
+Se o usuário digitar "s", o programa volta ao menu inicial. Caso contrário, ele exibirá uma mensagem de despedida e encerra:
+
+![obrigado](imgs/obrigado.png)
--- main.c
+++ main.c
@@ -30,55 +30,55 @@ int main() {
         printf("   Calculadora Simples\n");
         printf("===============================\n");
         printf("Selecione uma operação:\n");
-        printf("1. Adição\n");
-        printf("2. Subtração\n");
-        printf("3. Multiplicação\n");
-        printf("4. Divisão\n");
+        printf("1. Adicao\n");
+        printf("2. Subtracao\n");
+        printf("3. Multiplicacao\n");
+        printf("4. Divisao\n");
         printf("5. Sair\n");
-        printf("Opção: ");
+        printf("Opcao: ");
 
         resultado_scanf = scanf("%d", &op);
 
         if (resultado_scanf != 1) {
-            printf("\nErro: Opção inválida! Por favor, insira um número entre 1 e 5.\n\n");
+            printf("\nErro: Opcao invalida! Por favor, insira um numero entre 1 e 5.\n\n");
             while (getchar() != '\n'); 
             continue;
         }
 
         if (op == 5) {
-            printf("\nObrigado por usar a calculadora! Até a próxima.\n");
+            printf("\nObrigado por usar a calculadora! Ate a proxima.\n");
             break;
         }
 
         if (op < 1 || op > 5) {
-            printf("\nErro: Opção inválida! Por favor, insira um número entre 1 e 5.\n\n");
+            printf("\nErro: Opcao invalida! Por favor, insira um numero entre 1 e 5.\n\n");
             continue;
         }
 
         entrada_valida = 0;
 
-        printf("Digite o primeiro número: ");
+        printf("Digite o primeiro numero: ");
         while (!entrada_valida) {
             resultado_scanf = scanf("%lf", &num1); 
             
             if (resultado_scanf == 1) {            
                 entrada_valida = 1; 
             } else {
-                printf("Entrada inválida. Digite um número válido: ");
+                printf("Entrada invalida. Digite um numero valido: ");
                 while (getchar() != '\n'); 
             }
         }
 
         entrada_valida = 0; 
 
-        printf("Digite o segundo número: ");
+        printf("Digite o segundo numero: ");
         while (!entrada_valida) {
             resultado_scanf = scanf("%lf", &num2); 
             
             if (resultado_scanf == 1) {            
                 entrada_valida = 1; 
             } else {
-                printf("Entrada inválida. Digite um número válido: ");
+                printf("Entrada invalida. Digite um número valido: ");
                 while (getchar() != '\n'); 
             }
         }
@@ -99,7 +99,7 @@ int main() {
         } 
         else if (op == 4) {
             if (num2 == 0) {
-                printf("\nErro: Divisão por zero não é permitida.\n");
+                printf("\nErro: Divisao por zero nao e permitida.\n");
                 erro_divisao = 1;
             } else {
                 resultado = divisao(num1, num2);
@@ -113,17 +113,17 @@ int main() {
 
         int resposta_valida = 0;
         while (!resposta_valida) {
-            printf("\nDeseja realizar outra operação? (s/n): ");
+            printf("\nDeseja realizar outra operacao? (s/n): ");
             scanf(" %c", &continuar); 
 
             if (continuar == 's' || continuar == 'S') {
                 resposta_valida = 1;
                 printf("\n"); 
             } else if (continuar == 'n' || continuar == 'N') {
-                printf("\nObrigado por usar a calculadora! Até a próxima.\n");
+                printf("\nObrigado por usar a calculadora! Ate a proxima.\n");
                 return 0; 
             } else {
-                printf("Resposta inválida. Por favor, digite 's' para sim ou 'n' para não.\n");
+                printf("Resposta invalida. Por favor, digite 's' para sim ou 'n' para nao.\n");
             }
         }
     }
